6eec35f596c54d3d01e1f33cdeacf903a2bb8b22
Mayowa Ajiboye <mayaajiboye@gmail.com>
2022-12-22 20:34:45 -0500

    commit tree 585935909d89673c027eb276521b8a50eede0602

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1')
+
+newHeader.setAttribute('id', 'victory')
+
+newHeader.textContent = "MAYOWA is the champion";
